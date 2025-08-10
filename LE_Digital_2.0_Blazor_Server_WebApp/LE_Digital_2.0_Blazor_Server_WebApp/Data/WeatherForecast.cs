035358f086ef70cc59f6efe207c2a45f4bbc8885
Victor Filizola Teixeira <victor.filizola_teixeira@bbraun.com>
2025-08-10 19:13:33 -0300

    chore:initial commit

---
+++ LE_Digital_2.0_Blazor_Server_WebApp/LE_Digital_2.0_Blazor_Server_WebApp/Data/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace LE_Digital_2_Blazor_Server_WebApp.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
