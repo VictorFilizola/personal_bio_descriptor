ad964a25808929227f5cc3e60521b31e26b18cd2
Victor Filizola Teixeira <victor.filizola_teixeira@bbraun.com>
2025-09-24 05:44:31 -0300

    teste

--- LE_Digital_2.0_Blazor_Server_WebApp/LE_Digital_2.0_Blazor_Server_WebApp/Program.cs
+++ LE_Digital_2.0_Blazor_Server_WebApp/LE_Digital_2.0_Blazor_Server_WebApp/Program.cs
@@ -1,50 +1,43 @@
-using LE_Digital_2_Blazor_Server_WebApp.Server.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Authentication.Negotiate;
 using LE_Digital_2_Blazor_Server_WebApp.Core.Interfaces;
+using LE_Digital_2_Blazor_Server_WebApp.Infrastructure.Data;
 using LE_Digital_2_Blazor_Server_WebApp.Infrastructure.Services;
 using LE_Digital_2_Blazor_Server_WebApp.Server.Services;
+using Microsoft.AspNetCore.Authentication.Negotiate;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddDevExpressBlazor(options => {
-    options.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
-    options.SizeMode = DevExpress.Blazor.SizeMode.Medium;
-});
-builder.Services.AddSingleton<WeatherForecastService>();
-builder.WebHost.UseWebRoot("wwwroot");
-builder.WebHost.UseStaticWebAssets();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IVersionService, VersionService>();
-builder.Services.AddScoped<IEmailService, EmailService>();
-builder.Services.AddScoped<AppState>();
+// CORRECT: DbContext is registered here
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
-// 1. Add Authentication services and set the default scheme to Negotiate (Windows Auth)
+// CORRECT: Authentication is registered ONCE here
 builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
    .AddNegotiate();
 
 builder.Services.AddAuthorization(options =>
 {
-    // By default, all incoming requests will be authorized according to the default policy.
     options.FallbackPolicy = options.DefaultPolicy;
 });
-
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
-var app = builder.Build();
+// Register your custom services
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IVersionService, VersionService>();
+builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddScoped<AppState>();
 
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -52,8 +45,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
-// 2. Add the authentication and authorization middleware
-//    These MUST be between UseRouting() and MapBlazorHub()
+// Add the authentication and authorization middleware
 app.UseAuthentication();
 app.UseAuthorization();
 
