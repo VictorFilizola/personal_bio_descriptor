a8770b81c1c8a5969e9d5f4b2948ad9f86decd0e
Victor Filizola Teixeira <victor.filizola_teixeira@bbraun.com>
2025-09-24 05:31:43 -0300

    a

--- LE_Digital_2.0_Blazor_Server_WebApp/LE_Digital_2.0_Blazor_Server_WebApp/Server/Services/AppState.cs
+++ LE_Digital_2.0_Blazor_Server_WebApp/LE_Digital_2.0_Blazor_Server_WebApp/Server/Services/AppState.cs
@@ -1,6 +1,27 @@
-﻿namespace LE_Digital_2_Blazor_Server_WebApp.Server.Services
+﻿using System.Security.Claims;
+
+namespace LE_Digital_2_Blazor_Server_WebApp.Server.Services
 {
     public class AppState
     {
+        public ClaimsPrincipal? CurrentUser { get; private set; }
+        public ClaimsPrincipal? ImpersonatedUser { get; private set; }
+
+        public bool IsImpersonating => ImpersonatedUser != null;
+
+        public void SetCurrentUser(ClaimsPrincipal user)
+        {
+            CurrentUser = user;
+        }
+
+        public void StartImpersonation(ClaimsPrincipal userToImpersonate)
+        {
+            ImpersonatedUser = userToImpersonate;
+        }
+
+        public void StopImpersonation()
+        {
+            ImpersonatedUser = null;
+        }
     }
 }
