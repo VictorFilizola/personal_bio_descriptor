8a1d2d3834c335657a72e5fafc38af2220b46c6c
Victor Filizola Teixeira <victor.filizola_teixeira@bbraun.com>
2025-09-24 03:35:48 -0300

    Phase 2: Authentication and Application Shell

--- LE_Digital_2.0_Blazor_Server_WebApp/LE_Digital_2.0_Blazor_Server_WebApp/Program.cs
+++ LE_Digital_2.0_Blazor_Server_WebApp/LE_Digital_2.0_Blazor_Server_WebApp/Program.cs
@@ -1,7 +1,7 @@
 using LE_Digital_2_Blazor_Server_WebApp.Server.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-
+using Microsoft.AspNetCore.Authentication.Negotiate;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +16,22 @@ builder.Services.AddSingleton<WeatherForecastService>();
 builder.WebHost.UseWebRoot("wwwroot");
 builder.WebHost.UseStaticWebAssets();
 
+// 1. Add Authentication services and set the default scheme to Negotiate (Windows Auth)
+builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+   .AddNegotiate();
+
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to the default policy.
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -25,12 +40,15 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
 
+// 2. Add the authentication and authorization middleware
+//    These MUST be between UseRouting() and MapBlazorHub()
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
